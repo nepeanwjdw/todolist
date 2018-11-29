72278e61033043ce8d30f007950e822d138e96f6
Will Wayman <will.jdw@gmail.com>
2018-11-29 11:32:00 +0000

    deleting todos test passes

--- src/todoList.js
+++ src/todoList.js
@@ -2,5 +2,8 @@ function TodoList() {
   this.todos = []
 }
 TodoList.prototype.add = function(todo) {
-  this.todos.push(todo.name);
+  this.todos.push(todo);
+}
+TodoList.prototype.delete = function(todo) {
+  this.todos.splice(todo, 1);
 }
