1678b8b0dd7d2213882cdee33096f6ba788527ba
Will Wayman <will.jdw@gmail.com>
2018-11-29 11:31:45 +0000

    new test for deleting todos

--- spec/todoListSpec.js
+++ spec/todoListSpec.js
@@ -5,10 +5,18 @@ describe("TodoList", function() {
   beforeEach(function() {
     todoList = new TodoList();
     todo = new Todo("Go to Gym");
+    todo1 = new Todo("Post Letter");
   })
 
-  it("should allow a new todo to be created", function() {
+  it("should allow a new todo to be added to the todoList", function() {
     todoList.add(todo);
-    expect(todoList.todos).toEqual([todo.name]);
+    expect(todoList.todos).toEqual([todo]);
+  })
+
+  it("should allow a todo to be deleted from the todoList", function() {
+    todoList.add(todo);
+    todoList.add(todo1);
+    todoList.delete(todo);
+    expect(todoList.todos).toEqual([todo1]);
   })
 })
